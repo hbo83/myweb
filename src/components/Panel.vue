25f2cbbf1b8f3603add538a576b186684b8f4bf4
habi <hbo83@seznam.cz>
2019-12-28 15:38:10 +0100

    first commit

---
+++ src/components/Panel.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+    </div>   
+</template>
+
+<script>
+export default {
+    name: 'Panel',
+    components: {
+
+    },
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
